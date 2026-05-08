0825a7c0eaa4e49ddfee124714d4cb0bc314ee6d
John E <jeis4wpi@outlook.com>
2026-05-08 11:08:36 -0400

    add elfeed-org config
    Signed-off-by: John E <jeis4wpi@outlook.com>

--- init-local.el
+++ init-local.el
@@ -238,6 +238,20 @@
 ;; configure rss feed emacs including sciencedaily and slashdot and sacha chua
 (setq-default elfeed-search-filter "@6-months-ago +unread")
 
+;; Load elfeed-org
+(require 'elfeed-org)
+
+;; Initialize elfeed-org
+;; This hooks up elfeed-org to read the configuration when elfeed
+;; is started with =M-x elfeed=
+(elfeed-org)
+
+;; Optionally specify a number of files containing elfeed
+;; configuration. If not set then the location below is used.
+;; Note: The customize interface is also supported.
+(setq rmh-elfeed-org-files (list "~/.emacs.d/elfeed.org"))
+
+
 ;; emacs config enable swift mode init.el
 
 
